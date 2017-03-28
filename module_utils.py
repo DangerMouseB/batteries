7b7435da14fe4f2d43be90371bcbdbcadddd0762
DangerMouseB <david@Davids-MacBook.local>
2017-03-28 22:28:08 +0100

    changed license to apache 2.0

--- module_utils.py
+++ module_utils.py
@@ -1,21 +1,21 @@
-#************************************************************************************************************************************************
+#*******************************************************************************
 #
-#    Copyright (c) 2011 David Briant - see https://github.com/DangerMouseB
+#    Copyright (c) 2011-2012 David Briant
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
+#        http://www.apache.org/licenses/LICENSE-2.0
 #
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 #
-#************************************************************************************************************************************************
+#*******************************************************************************
+
 
 import sys
 
@@ -23,19 +23,19 @@ def printModules(root=''):
     noneNames = []
     moduleNames = []
     for k, v in sys.modules.items():
-        if k.find(root) == 0;
+        if k.find(root) == 0:
             if v is None:
                 noneNames.append(k)
             else:
                 moduleNames.append(k)
     noneNames.sort()
     moduleNames.sort()
-    print "****************** NONE ******************"
+    print("****************** NONE ******************")
     for mn in noneNames:
-        print mn
-    print "****************** MODULES ******************"
+        print(mn)
+    print("****************** MODULES ******************")
     for mn in moduleNames:
-        print mn
+        print(mn)
 
 def unloadIf(module_name, condition=True, leaveRelativeImportsOptimisation=False):
     # for description of relative imports optimisation in earlier versions of python see:
